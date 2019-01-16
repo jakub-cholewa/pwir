4e07f27e80267abe32479780c390b12ed69609c0
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-01-17 00:41:15 +0100

    fixed bugs

--- symulacja_skrzyzowania/src/cars_generator.erl
+++ symulacja_skrzyzowania/src/cars_generator.erl
@@ -5,7 +5,7 @@
 -include("../include/records.hrl").
 
 %% API
--export([start_link/1, init/1, terminate/2, code_change/3]).
+-export([start_link/1, init/1, terminate/2, code_change/3, handle_info/2, handle_cast/2, handle_call/3]).
 
 
 start_link([WorldParameters]) ->
@@ -15,6 +15,25 @@ start_link([WorldParameters]) ->
 init(State) ->
   erlang:start_timer(1000, self(), loop),
   {ok, State}.
+
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+handle_cast({add,Pos}, State) ->
+  Map = State#car_generator.map,
+  NMap = maps:update(Pos,maps:get(Pos,Map)+1,Map),
+  {noreply, State#car_generator{map = NMap}};
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+handle_info({timeout, _Ref, loop}, State)  ->
+  Map = State#car_generator.map,
+  NMap = Map,
+%%  NMap = generate(lists:filter(fun(X) -> maps:get(X,Map) > 0 end, maps:keys(Map)),State#car_generator.world_parameters,Map),
+  erlang:start_timer(1000, self(), loop),
+  {noreply, State#car_generator{map = NMap}};
+handle_info(_Info, State) ->
+  {noreply, State}.
 
 terminate(_Reason, _State) ->
   ok.
